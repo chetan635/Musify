6b2789c6425ffab6dc9c20d203688f5ae7add443
chetan635 <68587552+chetan635@users.noreply.github.com>
2021-11-10 15:29:22 +0530

    My Musify App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Leftnavbar from './Components/Leftnavbar';
+import Search from './Components/Search';
+import SearchByAlbum from './Components/SearchByAlbum';
+import { useState } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Playlist from './Components/Playlist';
 
 function App() {
+
+  // const [toggle, settoggle] = useState(true)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      
+        <div className="row">
+      <div className="col-md-3">
+        <Leftnavbar/>
+      </div>
+      <div className="col-md-9">
+        <Navbar/>
+        {/* <Search/> */}
+        <Switch>
+          <Route exact path="/">
+            <Search key="1"/>
+          </Route>
+          <Route  path="/playlist">
+              <Playlist/>
+          </Route>
+          <Route  path="/SearchByAlbum">
+              <SearchByAlbum/>
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
+    
   );
 }
 
